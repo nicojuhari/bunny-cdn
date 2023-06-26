2e4162b26223e9c15c8e074227386b612f8b4ed1
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-06-26 21:44:16 +0200

    adds google site verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+        head: { 
+            htmlAttrs: {
+                    lang: 'en'
+            },
+            title: 'Manage your Bunny.net assets with ease',
+            meta: [
+                { name: 'description', content: 'A better UI for bunny.net. Free and ease to use, no registration required' },
+                
+                { name: 'google-site-verification', content: "JmyDVPBCzxIuSYjRVpnFkZeD_Hm-bpKyrbpmHsfYNUw"}
+            ],
+        },
+    },
   modules: ['@nuxthq/ui', 'nuxt-icon', '@vueuse/nuxt'],
   css: ['@/assets/css/styles.css'],
   routeRules: {
