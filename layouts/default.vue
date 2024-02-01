58f8f9772e65e4b83fc0800efcf9afbd213d523b
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-02-01 21:55:19 +0100

    adds buttons redirect

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-col min-h-screen">
-        <IncludesHeader />
+        <!-- <IncludesHeader /> -->
         <div class="flex-auto">
             <slot></slot>
         </div>
-        <IncludesFooter class="mt-auto border-t" />
+        <!-- <IncludesFooter class="mt-auto border-t" /> -->
     </div>
 </template>
