55c10563b4ec137e0ff3e3f7825d5d8698b3038e
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-02-12 13:56:21 +0100

    fix the url

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,6 @@
 <template>
    <div class="grid place-content-center text-center h-screen">
         <div>We are moved to a new address</div>
-        <UButton to="https://apps.nicojuhari.com/bunny-cdn" class="mt-8 text-center justify-center">UI for Bunny.net CDN</UButton>
+        <UButton to="https://nicojuhari.com/tools/bunny-cdn" class="mt-8 text-center justify-center">UI for Bunny.net CDN</UButton>
    </div>
 </template>
